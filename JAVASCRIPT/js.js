833ead0bddde6bd460968819f04ae60f2c4232d3
Aatmika21 <100169564+Aatmika21@users.noreply.github.com>
2022-04-29 18:13:55 -0400

    Add files via upload

---
+++ JAVASCRIPT/js.js
@@ -0,0 +1,35 @@
+//alert("hello")
+
+$(document).mousemove (function(){
+//$(".flex-item").css("background-color","lightyellow")
+    if($(".flex-item1:hover").length==1){
+        $(".flex-item1").css("background-color","lightyellow")}
+         else{
+            $(".flex-item1").css("background-color","rgb(152, 224, 200);")
+        }
+        if($(".flex-item2:hover").length==1){
+            $(".flex-item2").css("background-color","lightblue")}
+             else{
+                $(".flex-item2").css("background-color","rgb(152, 224, 200);")
+            }
+            if($(".flex-item3:hover").length==1){
+                $(".flex-item3").css("background-color","lightpink")}
+                 else{
+                    $(".flex-item3").css("background-color","rgb(152, 224, 200);")
+                }
+                if($(".flex-item4:hover").length==1){
+                    $(".flex-item4").css("background-color","teal")}
+                     else{
+                        $(".flex-item4").css("background-color","rgb(152, 224, 200);")
+                    }
+                    if($(".flex-item5:hover").length==1){
+                        $(".flex-item5").css("background-color","green")}
+                         else{
+                            $(".flex-item5").css("background-color","rgb(152, 224, 200);")
+                        }
+                
+            
+        
+    
+    
+})  
